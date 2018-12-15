5ee98f8ba8a205f9091ff2d4329dca1f8f6c99c9
mariepastora <mpp2134@columbia.edu>
2018-12-15 18:00:18 -0500

    commit

--- src/06-map.js
+++ src/06-map.js
@@ -1,7 +1,7 @@
 import * as d3 from 'd3'
 import * as topojson from 'topojson'
 
-let margin = { top: 0, left: 0, right: 0, bottom: 0 }
+let margin = { top: 50, left: 50, right: 50, bottom: 50 }
 
 let height = 600 - margin.top - margin.bottom
 
@@ -12,11 +12,12 @@ let svg = d3
   .append('svg')
   .attr('height', height + margin.top + margin.bottom)
   .attr('width', width + margin.left + margin.right)
-  .style('background', 'black')
+  .style('background', '#02020E')
   .append('g')
   .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
 
-let projection = d3.geoAlbersUsa()
+let projection = d3.geoMercator().center([0, 41.83]).translate([width / 2, height / 2])
+
 
 // out geoPath needs a PROJECTION variable
 let path = d3.geoPath().projection(projection)
@@ -24,7 +25,7 @@ let path = d3.geoPath().projection(projection)
 Promise.all([
   d3.json(require('./pudding/us.topojson')),
   d3.csv(require('./pudding/state_bf.csv')),
-  d3.csv(require('./pudding/dots_for_map.csv'))
+  d3.csv(require('./pudding/dots_without_alaska.csv'))
 ])
   .then(ready)
   .catch(err => console.log('Failed on', err))
@@ -40,20 +41,24 @@ function ready([json, incarceration, allPoints]) {
 
   svg
     .append('g')
-    //.attr('transform', 'translate(-140,-10) scale(1.22)')
+    .attr('id', 'mapUSA')
+    .attr('transform', 'translate(-360,-1200) scale(5.4)')
     .selectAll('.state')
     .data(states.features)
     .enter()
     .append('path')
     .attr('class', 'state')
     .attr('d', path)
+    //.attr('fill', 'white')
     .attr('fill', 'rgba(0,0,0,0.01)')
     .on('mouseenter', function(d) {
       d3.select(this).attr('stroke', 'white')
+      .style('stroke-width', 0.5)
     })
     .on('mouseleave', function(d) {
       d3.select(this).attr('stroke', 'none')
     })
+
 
     /* Weird pudding stuff goes here */
   var radiusScale = d3.scaleSqrt()
@@ -68,15 +73,15 @@ function ready([json, incarceration, allPoints]) {
 
   let jailExtent = d3.extent(allPoints, d => +d.concentration)
 
-  var colorScale = d3.scaleSequential(d3.interpolatePlasma)
+  var colorScale = d3.scaleSequential(d3.interpolatePuRd)
     .domain(jailExtent)
     .clamp(true)
 
-  let xPositionScale = d3.scaleLinear().domain(xExtent).range([0, width*1.5])
-  let yPositionScale = d3.scaleLinear().domain(yExtent).range([height*1.5,0])
+  let xPositionScale = d3.scaleLinear().domain(xExtent).range([0, width])
+  let yPositionScale = d3.scaleLinear().domain(yExtent).range([height,0])
 
   svg.append('g')
-    .lower()
+    .raise()
     .selectAll('circle')
     .data(allPoints)
     .enter().append('circle')
@@ -84,7 +89,10 @@ function ready([json, incarceration, allPoints]) {
     .attr('cx', d => xPositionScale(+d.rounded_lng))
     .attr('cy', d => yPositionScale(+d.rounded_lat))
     .attr('fill', d => {
-      return colorScale(d.concentration*5)
+      return colorScale(d.concentration*7)
+    })
+    .on('mouseover', function(d){
+      console.log(d.rounded_lng)
     })
 
 }
