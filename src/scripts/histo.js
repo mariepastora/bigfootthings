9f4372d0668ccc47f389621acca6c2b108b71c83
Sophie Beiers <sbeiers@aclu.org>
2019-02-18 13:20:51 -0500

    adding enter-view and some COOL bar chart steps

--- src/scripts/histo.js
+++ src/scripts/histo.js
@@ -6,14 +6,13 @@ let height = 400 - margin.top - margin.bottom
 
 let width = 600 - margin.left - margin.right
 
-var svg = d3
+let svg = d3
   .select('#hist')
   .append('svg')
   .attr('height', height + margin.top + margin.bottom)
   .attr('width', width + margin.left + margin.right)
   .append('g')
   .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
-  .attr('background','white')
 
   var xPositionScale = d3.scaleBand().range([0, width])
   var heightScale = d3
@@ -43,19 +42,73 @@ function ready(datapoints) {
     .data(datapoints)
     .enter()
     .append('rect')
-    .attr('y', function(d) {
-      return height - heightScale(d['n'])
-    })
+    .classed("large", function(d) {
+    return d.token === 'large' })
+    .classed("hairy", function(d) {
+    return d.token === 'hairy'
+  })
+    .classed("tall", function(d) {
+    return d.token === 'tall'
+  })
+    .attr('y', height)
     .attr('x', function(d) {
       return xPositionScale(d.token)
     })
-    .attr('height', function(d) {
-      return heightScale(d.n)
-    })
     .attr('width', xPositionScale.bandwidth())
-    .attr('fill', function(d) {
-      return colorScale(d.token)
-    })
+    .attr('fill', 'transparent')
+
+
+  d3.select('#large').on('stepin', () => {
+      console.log("step 1")
+    svg
+      .selectAll('.large')
+      .transition()
+      .duration(500)
+      .attr('fill', function(d) {
+         return colorScale(d.token)
+       })
+      .attr('y', function(d) {
+        return height - heightScale(d['n'])
+      })
+      .attr('height', function(d) {
+        return heightScale(d.n)
+      })
+  })
+
+  d3.select('#tall').on('stepin', () => {
+      console.log("step 2")
+    svg
+      .selectAll('.tall')
+      .transition()
+      .duration(500)
+      .attr('fill', function(d) {
+         return colorScale(d.token)
+       })
+      .attr('y', function(d) {
+        return height - heightScale(d['n'])
+      })
+      .attr('height', function(d) {
+        return heightScale(d.n)
+      })
+  })
+
+  d3.select('#hairy').on('stepin', () => {
+      console.log("step 2")
+    // Grab all the bars, make them 0
+    svg
+      .selectAll('.hairy')
+      .transition()
+      .duration(500)
+      .attr('fill', function(d) {
+         return colorScale(d.token)
+       })
+      .attr('y', function(d) {
+        return height - heightScale(d['n'])
+      })
+      .attr('height', function(d) {
+        return heightScale(d.n)
+      })
+  })
 
   // var yAxis = d3.axisLeft(heightScale)
   // svg
